e0a196f667410c6a4126f4420c917a7215f019d4
Richard Chen <richardchen39@gmail.com>
2015-03-22 16:55:36 -0500

    Update Dijkstra.java

--- Dijkstra.java
+++ Dijkstra.java
@@ -2,6 +2,42 @@ import java.util.Arrays;
 
 public class Dijkstra {
 
+	private static void dijkstra(int[][] graph, int source) {
+		int[] dist = new int[graph.length];
+		int[] prev = new int[graph.length];
+		boolean[] visited = new boolean[graph.length];
+
+		Arrays.fill(dist, Integer.MAX_VALUE);
+		Arrays.fill(prev, -1);
+
+		int current = source;
+		dist[current] = 0;
+		while (current != -1) {
+			visited[current] = true;
+			for (int i = 0; i < graph.length; i++) {
+				if (graph[current][i] != 0) {
+					int cost = dist[current] + graph[current][i];
+					if (cost < dist[i]) {
+						dist[i] = cost;
+						prev[i] = current;
+					}
+				}
+			}
+
+			int next = -1, min = Integer.MAX_VALUE;
+			for (int i = 0; i < graph.length; i++) {
+				if (!visited[i] && dist[i] < min) {
+					next = i;
+					min = dist[i];
+				}
+			}
+
+			current = next;
+		}
+
+		System.out.println(Arrays.toString(dist));
+	}
+
 	public static void main(String[] args) {
 		int vertices = 6;
 		int[][] edges = { { 0, 1, 7 }, { 0, 2, 9 }, { 0, 5, 14 }, { 1, 2, 10 },
@@ -14,42 +50,7 @@ public class Dijkstra {
 			graph[edges[i][1]][edges[i][0]] = edges[i][2];
 		}
 
-		int[] dist = new int[vertices];
-		int[] prev = new int[vertices];
-		boolean[] visited = new boolean[vertices];
-		Arrays.fill(dist, Integer.MAX_VALUE);
-		Arrays.fill(prev, -1);
-
-		int current = 0;
-		int target = 4;
-		dist[current] = 0;
-
-		while (!visited[target] && current != -1) {
-			int adj = -1, min = Integer.MAX_VALUE;
-			for (int i = 0; i < vertices; i++) {
-				if (graph[current][i] == 0 || visited[i])
-					continue;
-
-				if (dist[current] + graph[current][i] < dist[i]) {
-					dist[i] = dist[current] + graph[current][i];
-					prev[i] = current;
-				}
-				if (dist[i] < min) {
-					adj = i;
-					min = dist[i];
-				}
-			}
-
-			visited[current] = true;
-			current = adj;
-		}
-
-		System.out.println(Arrays.toString(dist));
-
-		// path reconstruction
-		System.out.println(target);
-		for (; prev[target] != -1; target = prev[target])
-			System.out.println(prev[target]);
+		dijkstra(graph, 0);
 	}
 
 }
